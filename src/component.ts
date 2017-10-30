09da426f25d6f2adb3e99097da62f40b54850348
AbinayaSubbiah <abinaya.subbiah@syncfusion.com>
2017-10-30 15:00:32 +0530

    v4.0.6 release

--- src/component.ts
+++ src/component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-comp',
-    template: '<div>My First Component.</div>'
+    template: '<div>My First Component Initiated</div>'
 })
 export class MyComponent {
 
